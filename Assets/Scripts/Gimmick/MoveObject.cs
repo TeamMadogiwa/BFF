b491a2e997163c4d2416bdd50ed282d0795fa869
saito.ryouta <ryouta.saito@ta>
2018-06-11 18:50:54 +0900

    動くギミック作成途中 スペースでボールが初期値にもどるよ

---
+++ Assets/Scripts/Gimmick/MoveObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+
+	Transform cashedTransform;
+
+	[SerializeField]
+	bool deathFlg = false;
+
+	[SerializeField]
+	Vector2 movePow = new Vector2(1.0f, 1.0f);
+	[SerializeField]
+	Vector2 moveSpeed = new Vector2(1.0f, 1.0f);
+	[SerializeField]
+	Vector2 move = new Vector2(.0f, .0f);
+
+	public void Awake()
+	{
+		cashedTransform = this.transform;
+	}
+
+	public void Update()
+	{
+		MoveX(movePow.x, move.x, moveSpeed.x);
+	}
+
+	public void MoveX(float px, float x, float speed)
+	{
+		if(px <= .0f) return;
+
+		if(move.x <= -px) speed *= -1;
+		else if(move.x >= px) speed *= -1;
+
+		move += new Vector2(speed, .0f);
+		
+		cashedTransform.position += new Vector3 (speed, .0f, .0f);
+		
+	}
+}
--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class test : MonoBehaviour {
 
+	[SerializeField]
+	GameObject ball;
+	Vector3 ballInit = new Vector3(.0f, 3.0f, .0f);
+
 	// Use this for initialization
 	void Start () {
-		
+		ball.transform.position = ballInit;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKey(KeyCode.Space)){
+			ball.transform.position = ballInit;
+		}
 	}
 }
