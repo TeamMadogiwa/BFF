63d90d570f1584cb0d61f441d409493c123e3b83
reiya-shigeta <sakura.bass130@gmail.com>
2018-06-12 17:13:00 +0900

    広告表示をシングルトンで実装

---
+++ Assets/Scripts/Ads/UnityAds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class UnityAds : MonoBehaviour
+{
+	private static UnityAds mInstance;
+
+	private UnityAds(){}
+	public static UnityAds Instance
+	{
+		get{
+			if( mInstance == null )
+			{
+				GameObject obj = new GameObject("UnityAds");
+                mInstance = obj.AddComponent<UnityAds>();
+			}
+			return mInstance;
+		}
+		private set{}
+	}
+}
