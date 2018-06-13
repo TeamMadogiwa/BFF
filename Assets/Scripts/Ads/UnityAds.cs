23ed9efbcae87c31b837bba4bc6627d835725cc2
reiya-shigeta <sakura.bass130@gmail.com>
2018-06-13 11:32:51 +0900

    UnityAdsの基本処理

--- Assets/Scripts/Ads/UnityAds.cs
+++ Assets/Scripts/Ads/UnityAds.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
+using System;
 
 public class UnityAds : MonoBehaviour
 {
 	private static UnityAds mInstance;
+
+	private string gameID;
+
+	public Action callback = null;
 
 	private UnityAds(){}
 	public static UnityAds Instance
@@ -15,9 +20,38 @@ public class UnityAds : MonoBehaviour
 			{
 				GameObject obj = new GameObject("UnityAds");
                 mInstance = obj.AddComponent<UnityAds>();
+
+				if( Advertisement.isSupported )
+					Advertisement.Initialize(mInstance.gameID,true);
 			}
 			return mInstance;
 		}
 		private set{}
+	}
+
+	public void ShowRewardedVideo ()
+	{
+		if( !Advertisement.IsReady(gameID))
+		{
+			Debug.Log("広告のロードが終わっていません");
+		}
+		ShowOptions options = new ShowOptions();
+		options.resultCallback = HandleShowResult;	
+
+		Advertisement.Show(gameID, options);
+	}
+
+	public void HandleShowResult (ShowResult result)
+	{
+		if(result == ShowResult.Finished)
+		{
+			Debug.Log("Video completed - Offer a reward to the player");
+		}else if(result == ShowResult.Skipped)
+		{
+			Debug.LogWarning("Video was skipped - Do NOT reward the player");
+		}else if(result == ShowResult.Failed)
+		{
+			Debug.LogError("Video failed to show");
+		}
 	}
 }
