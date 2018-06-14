c11834b1085ae45fd242a33706f5b46e95e79282
reiya-shigeta <sakura.bass130@gmail.com>
2018-06-14 17:26:16 +0900

    #20 GimmikManagerをシングルトンクラスに変更

--- Assets/Scripts/Gimmick/GimmickManager.cs
+++ Assets/Scripts/Gimmick/GimmickManager.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GimmickManager : MonoBehaviour {
-
-	[SerializeField]
-	MoveObject move;
-	[SerializeField]
-	NeedleObject needle;
+public class GimmickManager : MonoBehaviour
+{
+	private static GimmickManager mInstance;
+	public static GimmickManager Instance
+	{
+		get{
+			if( mInstance == null )
+			{
+				GameObject obj = new GameObject("GimmickManager");
+                mInstance = obj.AddComponent<GimmickManager>();
+			}
+			return mInstance;
+		}
+		private set{}
+	}
 }
