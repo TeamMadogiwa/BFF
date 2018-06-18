35b14cfe79dddd05bd1bf5f2f9d0b58b5ecacb42
reiya-shigeta <sakura.bass130@gmail.com>
2018-06-18 14:49:39 +0900

    #4 ボール速度に上限値を設定

--- Assets/Scripts/Ball/Action.cs
+++ Assets/Scripts/Ball/Action.cs
@@ -2,23 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Action : MonoBehaviour {
-
+public class Action : MonoBehaviour
+{
 	Transform cashedTransform;
 
-	[SerializeField]
-	GameObject ball;
-	Vector3 ballInit = new Vector3(.0f, 3.0f, .0f);
-
+	private Rigidbody2D rigidbody;
+	
+	private readonly float MaxSpeed = 5.0f;
 	// Use this for initialization
-	void Start () {
-		ball.transform.position = ballInit;
+	void Start ()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(Input.GetKey(KeyCode.Space)){
-			ball.transform.position = ballInit;
+	void Update ()
+	{
+	}
+
+	/// <summary>
+	/// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void FixedUpdate()
+	{
+		if( rigidbody.velocity.magnitude > MaxSpeed )
+		{
+			rigidbody.velocity = rigidbody.velocity.normalized * MaxSpeed;
 		}
 	}
 }
